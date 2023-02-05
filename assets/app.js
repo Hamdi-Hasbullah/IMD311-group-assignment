502a2fec2e8e676a58bf74df8a49b87c42bb0049
sanseddy <sansedyy@gmail.com>
2023-02-05 18:03:30 +0800

    test

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,6 @@
+const faqs = document.querySelectorAll(".faq")
+const element = document.querySelector('.myP');
+
 //using arrow function to FAQs site
 //add event listener method to make event target for the toggle to make symbol change when user click
 faqs.forEach(faq => {
