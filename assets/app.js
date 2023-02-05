58c002b096f928e08b918bc4ee1797909be5ade3
sanseddy <sansedyy@gmail.com>
2023-02-05 16:50:29 +0800

    third commit

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+//using arrow function to FAQs site
+//add event listener method to make event target for the toggle to make symbol change when user click
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    })
+})
